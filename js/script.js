3304d568364e4e90233649e3684f1f3b961940df
UPEU <UPEU@172.17.1.1>
2017-04-26 21:40:26 -0500

    v1:version 1 del examen 01

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function demo(){
+    var pagina=document.getElementById("boton");
+    alert('hola desde el script');
+}
+function comentario(){
+    var com=document.getElementById("comentario");
+    alert('este comentario se enviara al servidor');
+    alert('tu comentario es: '+com.value);
+}
